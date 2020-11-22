739bac039795232fd5c038c556249258a20820b8
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2020-11-22 21:58:59 +0000

    fix icon path

--- core/src/de/fmr/main/Chunk.java
+++ core/src/de/fmr/main/Chunk.java
@@ -5,3 +5,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Chunk {
+	
+	private Tree[] t;
+	private int x, y;
+	public SpriteBatch b;
+	
+	public Chunk(int x, int y, SpriteBatch batch,  Tree[] t) {
+		
+		this.t = t;
+		this.x = x;
+		this.y = y;
+		b = batch;
+		
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
