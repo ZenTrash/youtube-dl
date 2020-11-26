20ae89248ff39a9e48d6465fe4362c97a2a8444d
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2020-11-26 09:07:30 +0000

    Update changlog

--- core/src/de/fmr/main/Chunk.java
+++ core/src/de/fmr/main/Chunk.java
@@ -23,3 +23,24 @@ public class Chunk {
 		return this.x;
 	}
 	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void render(Player Player) {
+		
+		for(Tree t : this.t) {
+			if(Player.getY() + Gdx.graphics.getHeight() / 2 <= t.getY() + 32) {
+				t.setRenderXY(t.getX() - Player.getX(), t.getY() - Player.getY());
+				t.render(b);
+			}
+		}
+		
+		
+		
+	}
+	
+	public void renderForeground(Player Player) {
+		
+		for(Tree t : this.t) {
+			if(Player.getY() + Gdx.graphics.getHeight() / 2 > t.getY() + 32) {
