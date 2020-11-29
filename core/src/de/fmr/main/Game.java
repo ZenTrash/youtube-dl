2faa4930742ea3236f1ebf7cfdb184b524f029bc
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2020-11-29 22:07:30 +0000

    README cleanup

--- core/src/de/fmr/main/Game.java
+++ core/src/de/fmr/main/Game.java
@@ -5,3 +5,27 @@ import java.util.Random;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Game {
+	
+	public static Texture TreeTexture = new Texture("Baum.png");
+	public static Random r = new Random();
+	
+	public static Tree[] chunkOut = null;
+	
+	public static Chunk[] Tout = new Chunk[MyGdxGame.RENDER_CHUNKS];
+	
+	
+	
+	
+		
+		
+	static Tree[] createChunk(int x, int y) {
+		
+		chunkOut = null;
+		
+		int a = r.nextInt(16) + 16;
+		
+		int lastY = y;
+		
+		int stepY = 500 / a;
