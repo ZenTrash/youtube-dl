55166b96ada6017d822c34508e696eb995f4bf6b
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2020-12-27 05:32:55 +0000

    update:

---
+++ core/src/de/fmr/main/MyGdxGame.java
@@ -0,0 +1,14 @@
+
+package de.fmr.main;
+
+
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
