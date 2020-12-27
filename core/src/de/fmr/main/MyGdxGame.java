9f7b0b4d2967966592da8c71bfa7bd1a2728d69e
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2020-12-27 12:35:57 +0000

    bug fixs

--- core/src/de/fmr/main/MyGdxGame.java
+++ core/src/de/fmr/main/MyGdxGame.java
@@ -12,3 +12,24 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	ShapeRenderer sp;
+	Player Player;
+	
+	public static final int RENDER_CHUNKS = 16;
+	
+	Chunk chunk;
+	Chunk[] chunks;
+	Game g;
+	
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		sp = new ShapeRenderer();
+		Player = new Player(Gdx.graphics.getWidth() / 2 - 32, Gdx.graphics.getHeight() / 2 - 32);
+		
+		
+		g = new Game();
