91af7b034848056a1be25ca8080e927babf31cbb
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-05-01 11:36:52 +0000

    missing quotes

--- core/src/de/fmr/main/Player.java
+++ core/src/de/fmr/main/Player.java
@@ -4,4 +4,4 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.grap
+import com.badlogic.gdx.graphics.g2d
