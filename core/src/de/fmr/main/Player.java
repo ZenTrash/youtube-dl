deb92d368850280cc22e409587dddf45a5bb7f16
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-10-23 10:04:05 +0000

    Bump to 0.2.0

--- core/src/de/fmr/main/Player.java
+++ core/src/de/fmr/main/Player.java
@@ -100,4 +100,4 @@ public class Player {
 			if(this.state != 3) this.state = 3;
 		}
 		
-		if(Gdx.input.isKeyPressed(Input.Keys.S) && Gdx.input.isKeyPressed(Input.Keys.D) &&
+		if(Gdx.input.isKeyPressed(Input.Keys.S) && Gdx.input.isKeyPressed(Input.Keys.D) && this.state != 1
