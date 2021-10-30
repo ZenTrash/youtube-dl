4c3feb1b2f9df8406845cd874af11e425823bd2f
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-10-30 12:59:50 +0000

    materials

--- core/src/de/fmr/main/Tree.java
+++ core/src/de/fmr/main/Tree.java
@@ -1,2 +1,22 @@
 
 package de.fmr.main;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Tree extends Object{
+	
+	
+
+	public Tree(int x, int y) {
+		super(x, y);
+		
+	}
+	
+	public void render(SpriteBatch b) {
+		b.begin();
+		b.draw(Game.TreeTexture, this.sx, this.sy, 128, 128);
+		b.end();
+	}
+
+}
