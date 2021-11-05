b19a14e8e7b27f984c93a3a7da58728d2290d202
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-11-05 22:04:18 +0000

    Remove compiled

--- desktop/src/de/fmr/main/DesktopLauncher.java
+++ desktop/src/de/fmr/main/DesktopLauncher.java
@@ -1,4 +1,5 @@
 package de.fmr.main;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationCo
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+impo
