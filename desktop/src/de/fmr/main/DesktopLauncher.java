ef7b62ddb2da69ac46d673116562f42e9f5a5430
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-11-05 18:21:14 +0000

    fixed missing bracket

--- desktop/src/de/fmr/main/DesktopLauncher.java
+++ desktop/src/de/fmr/main/DesktopLauncher.java
@@ -1,4 +1,4 @@
 package de.fmr.main;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwj
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationCo
