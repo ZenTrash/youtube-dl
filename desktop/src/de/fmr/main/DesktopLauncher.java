53dd2f4df77c633e2bf9ddc9b61124441aa7d7e6
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-11-10 19:42:59 +0000

    quick changes

--- desktop/src/de/fmr/main/DesktopLauncher.java
+++ desktop/src/de/fmr/main/DesktopLauncher.java
@@ -4,4 +4,4 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import de.fmr.main.MyGdxGame;
 
-// Please note that
+// Please note that on macOS your appl
