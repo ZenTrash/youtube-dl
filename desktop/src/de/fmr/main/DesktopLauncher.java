5df78311a61337c6762521dd82bb6b33a4e40567
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-11-10 06:25:30 +0000

    Update bash_aliases

--- desktop/src/de/fmr/main/DesktopLauncher.java
+++ desktop/src/de/fmr/main/DesktopLauncher.java
@@ -2,4 +2,6 @@ package de.fmr.main;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import de.fmr.main.
+import de.fmr.main.MyGdxGame;
+
+// Ple
