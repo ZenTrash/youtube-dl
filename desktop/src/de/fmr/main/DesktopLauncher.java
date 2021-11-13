e65d0595302911ec107ab6ae06927bf8a96b8941
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-11-13 21:33:20 +0000

    asserts

--- desktop/src/de/fmr/main/DesktopLauncher.java
+++ desktop/src/de/fmr/main/DesktopLauncher.java
@@ -4,4 +4,4 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import de.fmr.main.MyGdxGame;
 
-// Please note that on macOS your application needs to be started with th
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThrea
