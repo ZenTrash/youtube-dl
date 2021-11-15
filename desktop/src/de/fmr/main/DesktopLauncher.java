d3595820ba6fddac8a5d1b75aa3498a264dbed72
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-11-15 14:48:29 +0000

    Some more refactoring.

--- desktop/src/de/fmr/main/DesktopLauncher.java
+++ desktop/src/de/fmr/main/DesktopLauncher.java
@@ -4,4 +4,5 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import de.fmr.main.MyGdxGame;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argu
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public clas
