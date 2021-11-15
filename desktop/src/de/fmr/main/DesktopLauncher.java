230a8ccac45d6c73ba3f22528630099a1d8eaab5
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-11-15 20:44:51 +0000

    usb: core: buffer: smallest buffer should start at ARCH_DMA_MINALIGN

--- desktop/src/de/fmr/main/DesktopLauncher.java
+++ desktop/src/de/fmr/main/DesktopLauncher.java
@@ -5,4 +5,5 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import de.fmr.main.MyGdxGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
-public class DesktopLaunche
+public class DesktopLauncher {
+	public static v
