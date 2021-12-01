ceee5b716f09abef3f025744ff5d64d19abb97b1
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-12-01 19:28:53 +0000

    add base files

--- desktop/src/de/fmr/main/DesktopLauncher.java
+++ desktop/src/de/fmr/main/DesktopLauncher.java
@@ -8,4 +8,4 @@ import de.fmr.main.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		co
+		config.setForegrou
