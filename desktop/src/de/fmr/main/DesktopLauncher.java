7d60e0839698aedfa2703efa082e0502a11a9dd1
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-12-03 09:15:52 +0000

    Updated '_gallery/3-drawings.md' via CloudCannon

--- desktop/src/de/fmr/main/DesktopLauncher.java
+++ desktop/src/de/fmr/main/DesktopLauncher.java
@@ -9,4 +9,5 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("World
+		config.setTitle("World2Explore 2");
+		co
