3a5e630b06fc23f683f1ef1e53ba6eab7625086b
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2021-12-03 08:06:34 +0000

    Update coverage badge

--- desktop/src/de/fmr/main/DesktopLauncher.java
+++ desktop/src/de/fmr/main/DesktopLauncher.java
@@ -9,4 +9,4 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		con
+		config.setTitle("World
