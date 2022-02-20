5aaa83200a80dcd1102908770a35c9f25c5a32d9
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-02-20 22:43:32 +0000

    edited readme

--- gradlew.bat
+++ gradlew.bat
@@ -3,4 +3,4 @@
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
-@rem Y
+@rem You may obtain a copy 
