901742b7208c2b461891484bbc10e6569d0eb29b
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-03-02 19:35:03 +0000

    v1.5.0

--- gradlew.bat
+++ gradlew.bat
@@ -7,4 +7,5 @@
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
-@rem Unless required by applicable law or agreed to in writing
+@rem Unless required by applicable law or agreed to in writing, software
+@rem dist
