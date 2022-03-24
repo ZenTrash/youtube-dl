9e45232a327c494e4adda049a00bfb32dbe240ea
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-03-24 06:15:58 +0000

    Removed changelog

--- gradlew.bat
+++ gradlew.bat
@@ -12,3 +12,6 @@
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
+@rem
+
+@if "%DEB
