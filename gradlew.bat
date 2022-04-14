b849c4fec2b9453de7baabfaa37706260eac1c95
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-04-14 19:42:55 +0000

    Consistency.

--- gradlew.bat
+++ gradlew.bat
@@ -22,4 +22,4 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=
+if "%OS%"=="Windows_NT"
