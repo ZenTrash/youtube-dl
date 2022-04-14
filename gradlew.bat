4a43b557bba4a53fb5dd9b83f54d02406c878d38
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-04-14 17:52:36 +0000

    trying to fix travis

--- gradlew.bat
+++ gradlew.bat
@@ -21,4 +21,5 @@
 @rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with windows
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=
