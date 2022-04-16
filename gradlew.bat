1931feb813391e298b6c4bdbb7e5b04924480b7e
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-04-16 16:52:11 +0000

    Removed console.log

--- gradlew.bat
+++ gradlew.bat
@@ -22,4 +22,6 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT"
+if "%OS%"=="Windows_NT" setlocal
+
+set 
