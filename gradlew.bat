634e5d245f7ee0094265379b6b06622b4bd67c47
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-05-19 22:52:51 +0000

    v0.8.3

--- gradlew.bat
+++ gradlew.bat
@@ -32,4 +32,4 @@ set APP_HOME=%DIRNAME%
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JV
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to th
