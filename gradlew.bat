f785f099e8f8bb58bbd67802cb9ad8a7ccfa233d
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-06-26 18:32:17 +0000

    Update README-Myanmar.md

--- gradlew.bat
+++ gradlew.bat
@@ -59,4 +59,4 @@ if exist "%JAVA_EXE%" goto execute
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
-echo Please set the JAVA_HOME variable i
+echo Please set the JAVA_HOME variable in your envir
