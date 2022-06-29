443cdc8dd529d25298501f49c8ab71c30d6ea372
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-06-29 19:13:31 +0000

    version 0.3.4

--- gradlew.bat
+++ gradlew.bat
@@ -60,4 +60,5 @@ echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java ins
+echo location of your Java installation.
+
