479f38132b3d7ef3d99b352fceabf1a75ba2e6a1
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-08-12 09:46:33 +0000

    SELinux: include definition of new capabilities

--- gradlew.bat
+++ gradlew.bat
@@ -82,4 +82,5 @@ rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instea
 rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
-if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_
+if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
+exit /b %EXIT_
