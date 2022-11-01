9ad7f4e160f77053d0855889364460af13432f8e
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-11-01 16:24:04 +0000

    Update beta-appcast.xml

--- html/src/de/fmr/main/client/HtmlLauncher.java
+++ html/src/de/fmr/main/client/HtmlLauncher.java
@@ -2,4 +2,4 @@ package de.fmr.main.client;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
-import com.badlogic.gdx.b
+import com.badlogic.gdx.backends.gwt.G
