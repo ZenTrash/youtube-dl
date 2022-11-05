691e90e9ce5d20be5057defb415c07bcea4600ec
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-11-05 20:51:21 +0000

    fixed translations

--- html/src/de/fmr/main/client/HtmlLauncher.java
+++ html/src/de/fmr/main/client/HtmlLauncher.java
@@ -2,4 +2,5 @@ package de.fmr.main.client;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
-import com.badlogic.gdx.backends.gwt.GwtApplicationCon
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+im
