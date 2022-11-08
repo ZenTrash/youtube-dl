dced1ff85d1e3316fa3972828a374644de2aab6a
ZenTrash <101609412+ZenTrash@users.noreply.github.com>
2022-11-08 12:08:45 +0000

    cpufreq: interactive: Fix null pointer dereference in interactive governor

--- html/src/de/fmr/main/client/HtmlLauncher.java
+++ html/src/de/fmr/main/client/HtmlLauncher.java
@@ -3,4 +3,6 @@ package de.fmr.main.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import de.fmr.main.My
+import de.fmr.main.MyGdxGame;
+
+p
